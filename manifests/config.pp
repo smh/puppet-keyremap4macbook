5d04088eccc0a8ec3cd87d5e255c00b1f7728f09
Stein Martin Hustad <stein@hustad.com>
2013-04-01 08:58:42 +0200

    everything needs documentation

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,9 @@
+# configuration for keyremap4macbook
+#
+# $dmg_url - where to download the dmg
+# $app - location of installed application
+# $cli - location of command-line interface to the application
+# $private_xml_path - location of private.xml
 class keyremap4macbook::config {
   $dmg_url = 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg'
   $app = '/Applications/KeyRemap4MacBook.app'

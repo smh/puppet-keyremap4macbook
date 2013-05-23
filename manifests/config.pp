6d6fc73278659a0ac8a03ba7de92154ac31bdd60
Paul O'Connor <paul@poconnor.me>
2013-05-23 16:54:56 +0100

    Bump version to 8.3.0

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@
 # $cli - location of command-line interface to the application
 # $private_xml_path - location of private.xml
 class keyremap4macbook::config {
-  $dmg_url = 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg'
+  $dmg_url = 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.3.0.dmg'
   $app = '/Applications/KeyRemap4MacBook.app'
   $cli = "${app}/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli"
   $private_xml_path = "/Users/${::boxen_user}/Library/Application Support/KeyRemap4MacBook/private.xml"
